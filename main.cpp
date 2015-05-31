3dcf5949f5ddb3010e22ef6685776794ad0742c0
whileser <servulofonseca@gmail.com>
2015-05-30 23:09:19 -0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,17 @@
+/*
+ * File:   main.cpp
+ * Author: servulo/paulo/ELIVELTON
+ *
+ * Created on 30 de Maio de 2015, 22:05
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
