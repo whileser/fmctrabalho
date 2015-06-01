e8cf3ed55eb5be8a59a8609e9888c05e16fd0739
Paulo Sergio <paulosergios11@hotmail.com>
2015-06-01 11:55:25 -0300

    Adicionadas funções de MDC, Teorema Chines, Função Totiente de Euler

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include<stdlib.h>
 #include<ctype.h>
 #include<string.h>
+#include<math.h>
 
 typedef struct {
     char Mat[];
@@ -13,6 +14,68 @@ typedef struct{
 char row[];
 char posi[];
 }midi;
+
+// tirar o MDC de dois números inteiros.
+// retorna o Mínimo Divisor Comum (MDC)
+// caso retorne '1', os números são primos entre si.
+int euclidesMDC(int a, int b){
+    if(a > 0){
+        return euclidesMDC(b % a, a);
+    } else {
+      return b;
+    }
+}
+
+// checar se números são primos entre si dois a dois.
+// recebe um vetor de numeros para serem testados (numbers[]) e um tamanho do vetor (lines)
+// retorna 1 se todos os numeros do vetor forem coprimos entre si. 0 se existem 2 ou mais que não são coprimos.
+int Coprimes(int numbers[], int lines){
+    bool coprime = true;
+    int counter = 0;
+    while(coprime && counter < lines-1){
+        coprime = false;
+        for(int next = counter + 1; next < lines; next++){
+            if(euclidesMDC(numbers[counter], numbers[next]) == 1) // usa a função euclidesMDC() para tirar o MDC
+                coprime = true;
+        }
+        counter++;
+    }
+    if(coprime)
+        return 1;
+    return 0;
+}
+
+// Recebe um inteiro e retorna a sua função totiente
+long int getPhi(int number){
+    long int phi = 0;
+    for(int index = 0; index < number; index++){
+        if(euclidesMDC(index, number) == 1)
+            phi++;
+    }
+    return phi;
+}
+
+// Calcula a solução para um sistema de congruências usando TEOREMA CHINES DOS RESTOS
+
+/* 
+ * Recebe 2 vetores:
+ * -> um é o vetor dos coeficientes da congruencia (números aos quais x é congruente);
+ * -> outro é o vetor dos módulos de x;
+ * recebe também o tamanho dos vetores (length)
+ */
+long long int calculateX(int coefficients[], int modules[], int length){
+    long long int mod = 1; // Base to product
+    long long int result = 0;
+    for(int index = 0; index < length; index++){
+        mod *= modules[index];
+    }
+    // Chinese Remainder Theorem
+    // (x = a1*x1 + a2*x2 + ... + an * xn) mod (x1*x2*...*xn)
+    for(int index = 0; index < length; index++){
+        result += coefficients[index] * (pow(double (mod / modules[index]), getPhi(modules[index]))); // usa math.h e getPhi()
+    }
+    return result % mod;
+}
 
 int main() {
 
